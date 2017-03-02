983d810b67082d7574902a1f8c71e365354bbc8e
Vivek Pabani <vpabani@hawk.iit.edu>
2017-03-02 16:00:15 -0600

    Copy test function and test cases from first solution

--- largest-difference/Solution2.js
+++ largest-difference/Solution2.js
@@ -68,3 +68,29 @@ function swap(arr, i, j){
    arr[i] = arr[j];
    arr[j] = temp;
 }
+
+function testFindLargestDiff(arr, expected, testName) {
+
+    var actual;
+
+    actual = findLargestDiff(arr);
+
+    console.log("\n" + testName + "\n");
+    console.log("Array: " + arr);
+    console.log("Expected: " + expected);
+    console.log("Actual: " + actual);
+    console.log(expected==actual?"Passed":"Failed");
+
+}
+
+function test() {
+
+    testFindLargestDiff([3, 6, 4, 10, 9, 5], 7, "Random Array"); // 10 - 3
+    testFindLargestDiff([3, 5, 7, 11, 15, 17], 14, "Sorted Increasing Array"); // 17 - 3
+    testFindLargestDiff([17, 15, 11, 7, 5, 3], 14, "Sorted Decreasing Array"); // 17 - 3
+    testFindLargestDiff([3, -6, 4, 10, 9, -5], 16, "Random Array With Negative Values"); // 10 - (-6)
+    testFindLargestDiff([], -1, "Empty Array");
+    testFindLargestDiff([5], -1, "Single Item Array");
+}
+
+test();
