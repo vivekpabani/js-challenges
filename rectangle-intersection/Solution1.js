d5176da8cbc7d4e6f196977877d58726a49de941
Vivek Pabani <vpabani@hawk.iit.edu>
2017-03-02 16:02:31 -0600

    Implement function to check intersection

--- rectangle-intersection/Solution1.js
+++ rectangle-intersection/Solution1.js
@@ -5,3 +5,31 @@
   or top edge of one rectangle is below the bottom edge of another rectangle.
   If any of these two conditions is true, then they do not intersect, otherwise they intersect.
 */
+
+function doRectIntersect(rec1, rec2) {
+
+    var x1 = rec1.x,
+        y1 = rec1.y,
+        width1 = rec1.width,
+        height1 = rec1.height;
+
+    var x2 = rec2.x,
+        y2 = rec2.y,
+        width2 = rec2.width,
+        height2 = rec2.height;
+
+    // check if left edge of one rectangle is on the right of right edge of another rectangle.
+    if (x1 + width1 < x2 || x2 + width2 < x1) {
+
+        return false;
+    }
+
+    // check if top edge of one rectangle is below the bottom edge of another rectangle.
+    if (y1 + height1 < y2 || y2 + height2 < y1) {
+
+        return false;
+    }
+
+    return true;
+}
+
