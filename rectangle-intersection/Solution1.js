1f7c291b29d5ce34cac177e36bc7c574bce0cb98
Vivek Pabani <vpabani@hawk.iit.edu>
2017-03-02 16:01:46 -0600

    Update logic

---
+++ rectangle-intersection/Solution1.js
@@ -0,0 +1,7 @@
+
+/*
+  Logic:
+  two rectangles do not intersect if left edge of one rectangle is on the right of right edge of another rectangle,
+  or top edge of one rectangle is below the bottom edge of another rectangle.
+  If any of these two conditions is true, then they do not intersect, otherwise they intersect.
+*/
