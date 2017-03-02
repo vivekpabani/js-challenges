d0a3ca133407bcd52123b69002a3c72f7e3b6953
Vivek Pabani <vpabani@hawk.iit.edu>
2017-03-02 15:57:36 -0600

    Update logic for second solution

---
+++ largest-difference/Solution2.js
@@ -0,0 +1,11 @@
+
+/*
+  Logic:
+  Sort the array.
+  The largest difference is the difference between maximum and minimum value.
+  So return the difference of the min and max value,
+  which are the first and last element of the sorted array.
+
+  Since this algorithm takes O(nlogn) because of sorting, the other approach is better than this
+  which takes O(n) time and one pass through the array.
+*/
