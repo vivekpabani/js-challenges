def6f86e6a1e27e5fb11c3afe28064235da78cc3
Vivek Pabani <vpabani@hawk.iit.edu>
2017-03-02 15:56:36 -0600

    Create test cases for largestdiff

--- largest-difference/Solution1.js
+++ largest-difference/Solution1.js
@@ -49,3 +49,15 @@ function testFindLargestDiff(arr, expected, testName) {
     console.log(expected==actual?"Passed":"Failed");
 
 }
+
+function test() {
+
+    testFindLargestDiff([3, 6, 4, 10, 9, 5], 7, "Random Array"); // 10 - 3
+    testFindLargestDiff([3, 5, 7, 11, 15, 17], 14, "Sorted Increasing Array"); // 17 - 3
+    testFindLargestDiff([17, 15, 11, 7, 5, 3], 14, "Sorted Decreasing Array"); // 17 - 3
+    testFindLargestDiff([3, -6, 4, 10, 9, -5], 16, "Random Array With Negative Values"); // 10 - (-6)
+    testFindLargestDiff([], -1, "Empty Array");
+    testFindLargestDiff([5], -1, "Single Item Array");
+}
+
+test();
