8d9e8aeef0c04cadbdab336472b1787eb7db8018
Vivek Pabani <vpabani@hawk.iit.edu>
2017-03-02 15:55:28 -0600

    Create findLargestDiff function

--- largest-difference/Solution1.js
+++ largest-difference/Solution1.js
@@ -5,3 +5,33 @@
   The largest difference is the difference between maximum and minimum value.
   So return the difference of the min and max value.
 */
+
+function findLargestDiff(arr){
+
+    // at least two elements are required.
+    if (arr.length < 2) {
+
+        return -1;
+    }
+
+    var i = 0,
+        minVal = arr[0],
+        maxVal = arr[0];
+
+    for(i = 0; i < arr.length; ++i) {
+
+        // swap min with curr value if curr value is lower than min.
+        if (arr[i] < minVal) {
+            minVal = arr[i];
+        }
+
+        // swap max with curr value if curr value is higher than max.
+        else if (arr[i] > maxVal) {
+            maxVal = arr[i];
+        }
+    }
+
+    difference = maxVal - minVal;
+
+    return difference;
+}
