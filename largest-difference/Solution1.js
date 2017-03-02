0d612278b743e61f01296c22ad642968e72ed760
Vivek Pabani <vpabani@hawk.iit.edu>
2017-03-02 15:54:16 -0600

    Update Logic

---
+++ largest-difference/Solution1.js
@@ -0,0 +1,7 @@
+
+/*
+  Logic:
+  Iterate through the array and keep track of mimimum and maximum value so far.
+  The largest difference is the difference between maximum and minimum value.
+  So return the difference of the min and max value.
+*/
